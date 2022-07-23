b092d1b2d433fc53b8f2a2dc4f2d1dcc53fe46e1
aa <2668522945@qq.com>
2022-07-23 18:52:34 +0800

    blinn-phong

--- myRender/blinnFrag.glsl
+++ myRender/blinnFrag.glsl
@@ -50,8 +50,13 @@ void pointLight()
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    //phong
+    //vec3 reflectDir = reflect(-lightDir, norm);  
+    //float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    //blinn
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    float spec = pow(max(dot(norm, halfwayDir), 0.0), material.shininess);
+
     vec3 specular = light.specular * spec * texture(texture_specular1, TexCoords).rgb;  
     
     // attenuation
